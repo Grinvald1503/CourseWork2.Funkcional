d415e3bd493a61314d0f95018529eeaf01bc7795
Aleksandr <aleksanches123@mail.ru>
2023-02-07 13:37:37 +0300

    конец

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,6 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        String incomingText = "yourapp the quick brown fox jumps over the lazy dog";
-        Top10 countWords = new Top10(incomingText);
-        countWords.printResult();
     }
 
 
